637a827824e50c9430987a7b834521c137e72a13
WitheredWaste <134180639+WitheredWaste@users.noreply.github.com>
2026-06-04 07:48:02 +0000

    feat: add Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const inter = Inter({
@@ -72,6 +73,7 @@ export default function RootLayout({
           <Footer />
         </ThemeProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
